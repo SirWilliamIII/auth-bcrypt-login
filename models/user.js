f704e06ac3e41da78a9c7485d8debacbac5feca3
Will Carpenter <willcarpenteriii@gmail.com>
2017-11-05 22:43:48 -0700

    added delete route called on logout

--- models/user.js
+++ models/user.js
@@ -60,6 +60,19 @@ UserSchema.methods.generateAuthToken = function () {
 		})
 }
 
+UserSchema.methods.removeToken = function(token) {
+	const user = this
+
+	return user.update({
+		$pull: {
+			tokens: {
+				token
+			}
+		}
+	})
+
+}
+
 UserSchema.statics.findByToken = function (token) {
 	var User = this
 	let decoded;
